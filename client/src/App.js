a82f1b7c04f1ddfdfbb8f34bc08383aae2c88cba
Alex Patient <apatient97@gmail.com>
2022-05-20 19:18:52 +0100

    fetched API data and rendered. Written code for sorting ingredients and measures

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import FetchApi from './components/FetchDrinks';
 
 function App() {
   return (
     <div className='App'>
-
+      <FetchApi />
     </div>
   );
 }
---
+++ client/src/components/FetchDrinks/index.js
@@ -0,0 +1,74 @@
+import React, {useState, useEffect} from 'react'
+import './style.css'
+
+const FetchApi = () => {
+    const [data, setData] = useState([]);
+
+    const fetchDrinks = async () => {
+        const response = await fetch('https://www.thecocktaildb.com/api/json/v2/9973533/popular.php')
+        const result = await response.json()
+        // console.log(result)
+        setData(result.drinks)
+    }
+
+    useEffect(() => {
+        fetchDrinks()
+    }, [])
+
+   
+
+  return(
+    data.map(drink => {
+
+        const {strDrink, strDrinkThumb, strGlass, strAlcoholic, strInstructions} = drink
+
+        // Gather the ingredients for each drink
+        const ingArray = Object.entries(drink)
+        const ingFilter = ingArray.filter(x => x[0].includes('strIngredient') && x[1] !== null) 
+        // console.log(ingFilter)
+
+        // Gather the measures for each drink
+        let measuresArray = Object.entries(drink)
+        const mesFilter = measuresArray.filter(x => x[0].includes('strMeasure') && x[1] !== null)
+        console.log(mesFilter)
+        
+
+
+
+        return ( 
+           <div className="drink-card">              
+                <div className="drink-img">
+                    <h2>{strDrink}</h2>
+                    <img src={strDrinkThumb} alt={strDrink} className="drink-thumb"/>
+                </div>
+                <div className="drink-info">
+                    <p>{strGlass}</p>
+                    <p>{strAlcoholic}</p>
+                    <p>{strInstructions}</p>
+
+                <div className="grid">
+                    {ingFilter.map(ing => {
+                            return (
+                                <p className="left">{ing[1]}</p>
+                                
+                            )
+                        })}
+
+                    {mesFilter.map(mes => {
+                        return (
+                            <p className="right">{mes[1]}</p>
+                        )
+                    })}
+                </div>
+                       
+                                                     
+                </div>  
+            </div>
+        )
+    })
+  )  
+    
+      
+}
+
+export default FetchApi
