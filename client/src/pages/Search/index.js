1b29b7cd0eace95b2e6891055f8691047c4c47f4
Melmelg <melissa.gw.wong@gmail.com>
2022-05-25 23:19:49 +0100

    fixed search page button positions

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import NavigationButton from '../../components/NavigationButton';
 import { Outlet } from 'react-router-dom';
+import './style.css';
 
 const Search = () => {
   return (
     <>
-      <NavigationButton buttonName='Search by name' route='search/name'/> 
-      <NavigationButton buttonName='Search by ingredients' route='search/ingredient'/>
-      <Outlet />
+      <div className='search-page-container'>
+        <div className='search-page-button'>
+          <NavigationButton buttonName='Search by name' route='search/name'/> 
+          <NavigationButton buttonName='Search by ingredients' route='search/ingredient'/><Outlet />
+        </div>
+      </div>
+        
+      
     </>
   )
 }
